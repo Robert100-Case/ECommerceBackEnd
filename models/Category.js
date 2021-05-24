c9c159b6c7b5242c6e0fc7a01e54e9f51908415b
Bob <77537418+Robert100-Case@users.noreply.github.com>
2021-05-24 15:15:56 -0400

    Add files via upload

---
+++ models/Category.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+   category_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+   
+    category_name:  {
+      type: DataTypes.STRING
+  
+    
+    // define columns
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
